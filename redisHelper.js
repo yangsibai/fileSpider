0cf228b39faab91d653d3b6f880ca631ea5f6180
osrpt <yangsibai@gmail.com>
2014-03-08 21:23:53 +0800

    rewrite redis access

--- redisHelper.js
+++ redisHelper.js
@@ -5,6 +5,7 @@ var config = require('./config');
 var redis = require("redis"),
     client = redis.createClient(config.redisConfig.port, config.redisConfig.host);
 var utils = require('./utils');
+var uuid = require('node-uuid');
 
 /**
  * 记录异常
@@ -16,14 +17,28 @@ client.on('error', function (err) {
 /**
  *  压入url
  */
-exports.pushUrl = function (url) {
-    hasSpider(url, function (err, result) {
-        if (err) {
-            utils.error('redis push url error', err);
-        } else if (!result) {
-            client.sadd('urls', url, function (err, reply) {
-                if (err) {
-                    utils.error('redis sadd url error', err);
+exports.pushUrl = function (urls, callback) {
+    var guid = uuid.v4();
+    client.sadd(guid, urls, function (err, reply) {
+        if (err || reply === 0) {
+            client.del(guid);
+            if (typeof callback === 'function') {
+                callback();
+            }
+        } else {
+            client.sdiff(guid, 'spidered', function (err, reply) {
+                client.del(guid);
+                if (!err && reply) {
+                    client.sadd('urls', reply, function () {
+                        if (typeof callback === 'function') {
+                            callback();
+                        }
+                    });
+                }
+                else {
+                    if (typeof callback === 'function') {
+                        callback();
+                    }
                 }
             });
         }
@@ -35,35 +50,42 @@ exports.pushUrl = function (url) {
  * @param callback
  */
 exports.popUrl = function (callback) {
-    client.spop('urls', function (err, reply) {
-        if (!err && reply) {
-            addSpider(reply, function (err) {
-                callback(null, reply);
-            })
+    client.spop('urls', function (err, url) {
+        if (!err && url) {
+            client.sadd('spidered', url, function () {
+                callback(null, url);
+            });
         }
-    })
+    });
 };
 
-/**
- * 压入图片地址
- * @param url
- * @param @optional callback
- */
-exports.pushFileUrl = function (url, callback) {
-    hasDownloaded(url, function (err, result) {
-        if (!err && !result) {
-            client.sadd('files', url, function (err) {
-                if (typeof callback === 'function') {
-                    if (err) {
-                        callback(err);
-                    } else {
-                        callback(null);
+exports.pushFileUrl = function (urls, callback) {
+    var guid = uuid.v4();
+    client.sadd(guid, urls, function (err, reply) {
+        if (err || reply === 0) {
+            client.del(guid);
+            if (typeof callback === 'function') {
+                callback();
+            }
+        } else {
+            client.sdiff(guid, 'downloadedFile', function (err, reply) {
+                client.del(guid);
+                if (!err && reply) {
+                    client.sadd('files', reply, function () {
+                        if (typeof callback === 'function') {
+                            callback();
+                        }
+                    });
+                }
+                else {
+                    if (typeof callback === 'function') {
+                        callback();
                     }
                 }
             });
         }
-    })
-};
+    });
+}
 
 /**
  * pop一个文件地址
@@ -71,83 +93,14 @@ exports.pushFileUrl = function (url, callback) {
  * @return {[type]}            [description]
  */
 exports.popFileUrl = function (callback) {
-    client.spop('files', function (err, reply) {
-        if (!err && reply) {
-            addDownloadedFile(reply, function (err) {
-                callback(null, reply);
-            })
+    client.spop('files', function (err, url) {
+        if (!err && url) {
+            client.sadd('downloadedFile', url, function (err, reply) {
+                callback(null, url);
+            });
         }
     });
 }
-
-/**
- * 是否已经爬取
- * @param url
- * @param callback
- */
-exports.hasSpider = hasSpider = function (url, callback) {
-    client.sismember('hasSpider', url, function (err, reply) {
-        if (typeof callback === 'function') {
-            if (err) {
-                callback(err);
-            } else {
-                callback(null, reply);
-            }
-        }
-    })
-};
-
-/**
- * 添加已经爬取的地址
- * @param url
- * @param callback
- */
-exports.addSpider = addSpider = function (url, callback) {
-    client.sadd('hasSpider', url, function (err, reply) {
-        if (typeof callback === 'function') {
-            if (err) {
-                callback(err);
-            } else {
-                callback(null, reply);
-            }
-        }
-    })
-}
-
-/**
- * 检测文件是否已经下载过
- * @param  {[type]}   url  [description]
- * @param  {Function} callback [description]
- * @return {Boolean}           [description]
- */
-exports.hasDownloaded = hasDownloaded = function (url, callback) {
-    client.sismember('downloadedFile', url, function (err, reply) {
-        if (typeof callback === 'function') {
-            if (err) {
-                callback(err);
-            } else {
-                callback(null, reply);
-            }
-        }
-    })
-};
-
-/**
- * 添加已经下载过的文件信息
- * @param {[type]}   url      [description]
- * @param {Function} callback [description]
- */
-exports.addDownloadedFile = addDownloadedFile = function (url, callback) {
-    client.sadd('downloadedFile', url, function (err, reply) {
-        if (typeof callback === 'function') {
-            if (err) {
-                callback(err);
-            } else {
-                callback(null, reply);
-            }
-        }
-    })
-};
 
 /**
  * 显示状态
@@ -157,7 +110,7 @@ exports.displayStatus = displayStatus = function () {
     setInterval(function () {
         client.scard('urls', function (err, urlsCount) {
             client.scard('files', function (err, filesCount) {
-                client.scard('hasSpider', function (err, spideredCount) {
+                client.scard('spidered', function (err, spideredCount) {
                     client.scard('downloadedFile', function (err, downloadedCount) {
                         console.log('status => urls = ' + urlsCount + " files = " + filesCount + "  spidered = " + spideredCount + "  downloaded = " + downloadedCount);
                     });
