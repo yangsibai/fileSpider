a9690a09ae202ca360463d2cbc7fcbdce41b8ef7
osrpt <yangsibai@gmail.com>
2014-03-08 21:23:12 +0800

    add config:keep original file name

--- config.js
+++ config.js
@@ -8,11 +8,12 @@ exports.redisConfig = {
 };
 
 exports.taskConfig = {
-    startUrl: 'http://tieba.baidu.com/f?kw=%C4%A7%CA%DE%CA%C0%BD%E7&fr=index',
-    sitePattern: /tieba\.com/i,
-    filePattern: /(\.jpg|\.png|\.gif)/i,
-    minSizeByKb: 20,
-    maxSizeBykb: 20480
+    startUrl: 'http://zhihu.com',
+    sitePattern: /zhihu\.com/i,
+    filePattern: /(\.[a-z0-9]{1,5})$/i,
+    minSizeByKb: 30,
+    maxSizeBykb: 20480,
+    keepName: true
 };
 
 /**
@@ -20,7 +21,7 @@ exports.taskConfig = {
  * @type {{on: boolean, host: string, port: number}}
  */
 exports.proxyConfig = {
-    on: false,
+    on: true,
     host: '127.0.0.1',
     port: 8087
 };
